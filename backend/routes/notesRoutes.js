8341706abea6db5b6f54b112cd294bbb44438697
kalash <kalash21504@gmail.com git config --global user.email kalash21504@gmail.com>
2025-06-29 19:03:10 +0530

    first commit

---
+++ backend/routes/notesRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"; //used with type=module
+// const express=require("express"); //used with type=common
+import { getAllnotes, createNote, updateNote, deleteNote} from "../../controllers/notesController.js"
+
+const router=express.Router();
+
+router.get("/", getAllnotes);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+
+export default router; //used with type=module
+// module.exports = router;  //used with type=common
